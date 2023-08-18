0043aeed75df6ad2189713a99553f0f386075609
ZLoo <Kozlov.s.v.1992@gmail.com>
2023-08-18 19:17:19 +0300

    Update scss add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import { Application } from './Application';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<Application />);
+root.render(
+  <StrictMode>
+    <Application />
+  </StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
