94929960aa032668f41d63ac6c54959102d58313
ZLoo <Kozlov.s.v.1992@gmail.com>
2023-08-18 20:04:38 +0300

    Rolling back the change

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,7 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <StrictMode>
-    <Application />
-  </StrictMode>,
-);
+root.render(<Application />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
