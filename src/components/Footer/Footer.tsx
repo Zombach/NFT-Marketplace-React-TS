373dfa971cb7176ede22c4af29d4a3b284f8241d
ZLoo <Kozlov.s.v.1992@gmail.com>
2023-08-19 13:44:37 +0300

    Move footer components to ./components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 import './Footer.scss';
 import { ReactComponent as Background } from './assets/Background.svg';
 import { ReactComponent as Delimiter } from './assets/Delimiter.svg';
-import AboutUs from './AboutUs/AboutUs';
-import Copyright from './Copyright/Copyright';
-import Links from './Links/Links';
+import AboutUs from './components/AboutUs/AboutUs';
+import Copyright from './components/Copyright/Copyright';
+import Links from './components/Links/Links';
 import React, { type FC } from 'react';
-import SocialNetwork from './SocialNetwork/SocialNetwork';
-import Subscribe from './Subscribe/Subscribe';
-import Trust from './Trust/Trust';
+import SocialNetwork from './components/SocialNetwork/SocialNetwork';
+import Subscribe from './components/Subscribe/Subscribe';
+import Trust from './components/Trust/Trust';
 
 export const Footer: FC = () => {
   return (
