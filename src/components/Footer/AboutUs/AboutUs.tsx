0dd170ed019d57b0b4569c6326d6b14abdbfb9b5
ZLoo <Kozlov.s.v.1992@gmail.com>
2023-08-19 10:31:13 +0300

    Text enhancement

--- src/components/Footer/AboutUs/AboutUs.tsx
+++ src/components/Footer/AboutUs/AboutUs.tsx
@@ -4,7 +4,7 @@ import React, { type FC } from 'react';
 export const AboutUs: FC = () => {
   return (
     <div className="about-us">
-      <h2 className="h2">Our platform is trusted by millions,& features an portfolio of financial product offerings.</h2>
+      <h2 className="h2">Our platform is trusted by millions & features a portfolio of financial product offerings.</h2>
       <h3 className="h3">NFT-Marketplace-React-TS@gmail.com</h3>
     </div>
   );
