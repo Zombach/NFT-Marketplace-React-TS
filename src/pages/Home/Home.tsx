dfca5a12aa9881b1824adefd1b679d2aa0919ffb
ZLoo <Kozlov.s.v.1992@gmail.com>
2023-08-20 11:47:20 +0300

    remove font-style: normal

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import './Home.scss';
 import React, { type FC } from 'react';
 
 export const Home: FC = () => {
-  return <div className=""></div>;
+  return <div className="">Home</div>;
 };
 
 export default Home;
