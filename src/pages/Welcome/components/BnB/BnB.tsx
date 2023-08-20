5737d216ddee3cdafcd487599f420cf74bcd7cc8
ZLoo <Kozlov.s.v.1992@gmail.com>
2023-08-20 10:18:38 +0300

    Welcome page completed

--- src/pages/Welcome/components/BnB/BnB.tsx
+++ src/pages/Welcome/components/BnB/BnB.tsx
@@ -1,9 +1,9 @@
 import './BnB.scss';
 import { ReactComponent as Background } from './assets/background.svg';
-import { ReactComponent as BackgroundImg } from './assets/background-img.svg';
-import { ReactComponent as BnBBase } from './assets/b-n-b-base.svg';
-import { ReactComponent as BnBMini } from './assets/b-n-b-mini.svg';
+import { ReactComponent as BnBBase } from './assets/bnb-base.svg';
+import { ReactComponent as BnBMini } from './assets/bnb-mini.svg';
 import { ReactComponent as Img } from './assets/img.svg';
+import PictureBackground from './assets/background.jpg';
 import React, { type FC } from 'react';
 
 interface BnBProps {
@@ -14,20 +14,19 @@ export const BnB: FC<BnBProps> = ({ className }) => {
   return (
     <div className={className}>
       <Background className="background" />
+      <img className="picture-background" src={PictureBackground} alt="p-b" />
       <div className="picture-block">
         <Img className="picture" />
-        <BackgroundImg className="background" />
+        <BnBMini className="bnb-mini" />
+        <BnBBase className="bnb-base part-1" />
+        <BnBBase className="bnb-base part-2" />
       </div>
       <div className="text-block">
         <h3 className="h3">Buy Crypto in Minutes</h3>
-        <span className="t">Enjoy the world’s cryptocurrency exchange at your fingertips</span>
+        <span className="text">Enjoy the world’s cryptocurrency exchange at your fingertips</span>
       </div>
     </div>
   );
 };
 
 export default BnB;
-
-/*
-<Background className="background" />     
-*/
