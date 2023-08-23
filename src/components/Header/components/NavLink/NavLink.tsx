cbffdf0676a3cfd764631b7402fc46dd604d08c2
saenkaaa <saenkaaa.riki@gmail.com>
2023-08-23 11:46:21 +0300

    fixed color and font

--- src/components/Header/components/NavLink/NavLink.tsx
+++ src/components/Header/components/NavLink/NavLink.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
 import './NavLink.scss';
+import React from 'react';
 
-export  const NavLink = () => {
-    return (
-      <div className="link">
-        <a href="welcome" className="link-text">
-          Art
-        </a>
-        <div>/</div>
-        <a href="welcome" className="link-text">
-          Community
-        </a>
-        <div>/</div>
-        <a href="welcome" className="link-text">
-          Magazine
-        </a>
-        <div>/</div>
-        <a href="welcome" className="link-text">
-          Shop
-        </a>
-      </div>
-    );
+export const NavLink = () => {
+  return (
+    <div className="link">
+      <a href="welcome" className="link-text">
+        Art
+      </a>
+      <div>/</div>
+      <a href="welcome" className="link-text">
+        Community
+      </a>
+      <div>/</div>
+      <a href="welcome" className="link-text">
+        Magazine
+      </a>
+      <div>/</div>
+      <a href="welcome" className="link-text">
+        Shop
+      </a>
+    </div>
+  );
 };
 export default NavLink;
