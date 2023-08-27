88486e83f4059571513e9d64eae61ceb7588d788
VitalyOv <vit2013aly@mail.ru>
2023-08-27 17:37:46 +0300

    Added video component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,13 @@
 import './Home.scss';
 import { UserContext } from '@components/Contexts/UserContext';
+import { Video } from '@components/Video/Video';
 import React, { type FC, useContext } from 'react';
 
 export const Home: FC = () => {
   const context = useContext(UserContext);
   return (
-    <div className="">
-      <p>Id: {context?.id}</p>
-      <p>UserName: {context?.name}</p>
-      <p>Token: {context?.token}</p>
+    <div>
+      <Video></Video>
     </div>
   );
 };
