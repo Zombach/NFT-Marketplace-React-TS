e3f3b3d302be15cc96b29b50806947ebae69df6f
saenkaaa <saenkaaa.riki@gmail.com>
2023-08-27 18:43:35 +0300

    added input component

--- src/pages/LogIn/components/EmailPassword/EmailPassword.tsx
+++ src/pages/LogIn/components/EmailPassword/EmailPassword.tsx
@@ -1,7 +1,7 @@
 import './EmailPassword.scss';
 import { ContinueWith } from '../ContinueWith/ContinueWith';
 
-import { ReactComponent as EyeClosed } from './assets/eyeClosed.svg';
+import { InputText } from '../InputText/InputText';
 import { TapHere } from '../TapHere/TapHere';
 import React, { type FC } from 'react';
 
@@ -9,23 +9,8 @@ export const EmailPassword: FC = () => {
   return (
     <form className="left-block">
       <h2 className="text">Log in</h2>
-      <fieldset>
-        <legend>
-          Email / Phone number <span className="red-color">*</span>
-        </legend>
-        <div className="table">bnb1234@gmail.com</div>
-      </fieldset>
-      <fieldset>
-        <legend>
-          Password <span className="red-color">*</span>
-        </legend>
-        <div className="table">
-          <div className="left-column">**************</div>
-          <div className="right-column">
-            <EyeClosed />
-          </div>
-        </div>
-      </fieldset>
+      <InputText name="Email / Phone number *" value="Your email" />
+      <InputText name="Password *" value="Your password" eye="eye-need" />
       <TapHere text="Forget password?" tap="Tap here" />
       <button className="button-fill">
         <p className="text-b">Log In</p>
