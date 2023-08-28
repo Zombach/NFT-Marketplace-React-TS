6bf35f82b4e43aed447793463fe49480fd5a781b
m_maksina <m_maksinaa@mail.ru>
2023-08-28 18:50:52 +0300

    fix

--- src/pages/Checkout/components/CheckoutFragment/CheckoutFragment.tsx
+++ src/pages/Checkout/components/CheckoutFragment/CheckoutFragment.tsx
@@ -5,11 +5,11 @@ import React, { type FC } from 'react';
 export const CheckoutFragment: FC = () => {
   return (
     <div className="checkout-accordions-container">
-      <AccordionCheckout title="Customers Information" content={'jopa'} />
-      <AccordionCheckout title="Delivery" content={'jopa'} />
-      <AccordionCheckout title="Payment" content={'jopa'} />
-      <AccordionCheckout title="Additional info" content={'jopa'} />
-      <AccordionCheckout title="Summary" content={'jopa'} />
+      <AccordionCheckout title="Customers Information" content={'j'} />
+      <AccordionCheckout title="Delivery" content={'j'} />
+      <AccordionCheckout title="Payment" content={'j'} />
+      <AccordionCheckout title="Additional info" content={'j'} />
+      <AccordionCheckout title="Summary" content={'j'} />
     </div>
   );
 };
