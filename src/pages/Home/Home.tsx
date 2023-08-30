b227e04d1e66e1c5c633513c9982d74d97ecd7f5
saenkaaa <saenkaaa.riki@gmail.com>
2023-08-30 19:06:16 +0300

    returned back the video

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,13 @@
 import './Home.scss';
-import { RadioButton } from '@components/RadioButton/RadioButton';
 import { UserContext } from '@components/Contexts/UserContext';
+import { Video } from '@components/Video/Video';
 import React, { type FC, useContext } from 'react';
 
 export const Home: FC = () => {
   const context = useContext(UserContext);
   return (
-    <div className="choose-pay">
-      <RadioButton textLeft="PayPal" textRight="+ $14.00" />
-      <RadioButton textLeft="ApplePal" textRight="+ $19.00" />
-      <RadioButton textLeft="GooglePal" />
+    <div>
+      <Video></Video>
     </div>
   );
 };
