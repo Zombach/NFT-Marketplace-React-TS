b6b941078c0fda63e88ee3b43f1373220d8443f6
saenkaaa <saenkaaa.riki@gmail.com>
2023-09-14 22:38:04 +0300

    add checkbox

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,12 +1,17 @@
 import './SignUp.scss';
 import { EmailPassword } from '@pages/LogIn/components/EmailPassword/EmailPassword';
 import { ReactComponent as Foto } from './assets/foto.svg';
-import { InputText } from '@components/InputText/InputText';
+
 import { ReactComponent as Line } from './assets/line.svg';
 import { TapHere } from '@pages/LogIn/components/TapHere/TapHere';
-import React, { type FC } from 'react';
+import InputText from '@components/Inputs/InputText/InputText';
+import React, { useState, type FC } from 'react';
+import Checkbox from '@components/Checkbox/Checkbox';
 
 export const SignUp: FC = () => {
+  
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+
   return (
     <div id="sign-up-section">
       <form className="left-block">
@@ -14,11 +19,14 @@ export const SignUp: FC = () => {
         <div className="login-container">
           <InputText id="email" name="email" placeholder="Email / Phone number" isRequired={true} />
           <InputText id="password" name="password" placeholder="Password" isRequired={true} />
-          <InputText id="refferal" name="refferal" placeholder="Refferal ID:" isRequired={false} className="input-group grey" />
+          <InputText id="refferal" name="refferal" placeholder="Refferal ID:" isRequired={false} className="input-group opacity-refferal" />
         </div>
-        <div className="one-line">
-          <TapHere text="I have read and agree to BnB " tap="Terms of Service" />
-          <TapHere text="and " tap="Private Policy" />
+        <div>
+          <Checkbox
+            label={'I have read and agree to BnB Terms of Service and Private Policy'}
+            onChange={(e) => {
+              setIsChecked(e.target.checked);
+            }}></Checkbox>
         </div>
         <button className="button-fill">
           <p className="text-b">Create personal account</p>
